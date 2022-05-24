fbae1ba15cc0b84ff7aec4510c36ff7962ab3d3f
printSANO <leehahnwoong@gmail.comm>
2022-05-23 23:18:35 -0700

    [c]

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ bot=commands.Bot(command_prefix='./')
 
 @bot.event
 async def on_ready():
-    print('로그인중입니다. ')
-    print(f"봇={bot.user.name}로 연결중")
-    print('연결이 완료되었습니다.')
+    print('Connecting ')
+    print(f"Connecting to {bot.user.name} BOT")
+    print('Connedtion Success')
     await bot.change_presence(status=discord.Status.online, activity=None)
     
-bot.run(Token)#보안을위해 다른 코드(to.py)에서 토큰값을 가져옴.
+bot.run(Token)
